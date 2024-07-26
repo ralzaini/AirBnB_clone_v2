d6b53afed69c14badf23ac7cbec09186b9d24549
ralzaini <rehabalzaini@gmail.com>
2024-07-26 11:58:35 -0700

    aaw

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Hello module is a simple module to say hello
+"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_hbnb():
+    """
+    hello hbnb
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """ display hbnb """
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c_with_params(text):
+    """ display path"""
+    text_n = text.replace('_', ' ')
+    return "C {}".format(text_n)
+
+
+@app.route("/python", defaults={'text': 'is cool'})
+@app.route("/python/<text>")
+def python_with_params(text):
+    """ display path python"""
+    text_n = text.replace('_', ' ')
+    return 'python {}'.format(text_n)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
