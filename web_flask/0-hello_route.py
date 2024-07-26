e166e93d3c7b2aae236a6f27be5a2f0185a0e519
ralzaini <rehabalzaini@gmail.com>
2024-07-26 09:02:48 -0700

    aaw

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,13 +6,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
 
+@app.route('/', strict_slashes=False)
 def home():
     """
     flask app
     """
     return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
