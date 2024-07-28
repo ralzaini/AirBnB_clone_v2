1b6fa66d504bba949e3e4c509acdbb0d382732d4
ralzaini <rehabalzaini@gmail.com>
2024-07-28 14:37:33 +0300

    wwww

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,25 +1,29 @@
 #!/usr/bin/python3
 """
-starts a Flask web application
+Hello module is a simple module to say hello
 """
 
+
 from flask import Flask, render_template
-from models import *
+from markupsafe import escape
 from models import storage
+from models.state import State
+
+
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """display a HTML page with the states listed in alphabetical order"""
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
-    return render_template('7-states_list.html', states=states)
-
-
 @app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage on teardown"""
+def teardown_db(exception=None):
+    """ Close session of Database """
     storage.close()
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+
+@app.route("/states_list", strict_slashes=False)
+def get_states_route():
+    """ get the states list """
+    return render_template("7-states_list.html", states=storage.all(State))
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
