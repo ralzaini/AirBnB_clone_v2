014052685bce70812ab489f37c2d63b93a33fc60
ralzaini <rehabalzaini@gmail.com>
2024-07-28 19:23:06 +0300

    ssss

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,29 +1,25 @@
 #!/usr/bin/python3
 """
-Hello module is a simple module to say hello
+start Flask application
 """
 
-
 from flask import Flask, render_template
+from models import *
 from models import storage
-from models.state import State
-
-
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def teardown_db(exception=None):
-    """ Close session of Database """
-    storage.close()
-
-
-@app.route("/states_list", strict_slashes=False)
-def get_states_route():
-    """ get the states list """
-    states = storage.all(State)
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """display a HTML page with the states listed in alphabetical order"""
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
     return render_template('7-states_list.html', states=states)
 
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage on teardown"""
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
